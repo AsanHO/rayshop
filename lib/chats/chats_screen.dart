ecb22c26d7701bbbeb7940a03127b5e242997b69
whgustj00 <whgustj00@naver.com>
2023-05-25 17:13:45 +0900

    5/25 detail screen

--- lib/chats/chats_screen.dart
+++ lib/chats/chats_screen.dart
@@ -38,13 +38,13 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
+              children: [
                 ChatCategory(text: '전체'),
                 ChatCategory(text: '진행중'),
                 ChatCategory(text: '진행 예정'),
