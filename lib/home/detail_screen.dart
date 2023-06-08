9b64fc9676a0d1af970bb91b27c1a2b211a20e85
whgustj00 <whgustj00@naver.com>
2023-06-08 15:11:20 +0900

    06/08 Expired -> 입찰마감

--- lib/home/detail_screen.dart
+++ lib/home/detail_screen.dart
@@ -76,7 +76,7 @@ class _DetailScreenState extends State<DetailScreen> {
 
     if (difference.isNegative) {
       setState(() {
-        timeRemaining = 'Expired';
+        timeRemaining = '입찰 마감';
         isEnd = true;
       });
       return;
@@ -281,7 +281,7 @@ class _DetailScreenState extends State<DetailScreen> {
                             Text(
                               timeRemaining,
                               style: const TextStyle(
-                                  fontSize: 38,
+                                  fontSize: 34,
                                   fontWeight: FontWeight.w600,
                                   color: Colors.orange),
                             ),
